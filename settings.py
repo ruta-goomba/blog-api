f63a534af5f861f31fafffa8dcba543bb7172214
ruta.sakalauskaite@qustodio.com <ruta.sakalauskaite@qustodio.com>
2017-08-20 15:59:21 +0200

    basic api backend setup

---
+++ settings.py
@@ -0,0 +1,28 @@
+RESOURCE_METHODS = ['GET', 'POST']
+DOMAIN = {
+  'user': {
+    'schema': {
+      'firstname': {
+        'type': 'string'
+      },
+      'lastname': {
+        'type': 'string'
+      },
+      'username': {
+          'type': 'string',
+           'unique': True
+      },
+      'password': {
+          'type': 'string'
+      },
+      'phone': {
+          'type': 'string'
+      }
+        }
+    },
+  'additional_lookup': {
+    'url': 'regex("[\w]+")',
+    'field': 'username',
+  }
+
+}
