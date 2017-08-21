38f586eabd2a82dd0d50eee2b0fdef5038a3023c
ruta.sakalauskaite@qustodio.com <ruta.sakalauskaite@qustodio.com>
2017-08-21 15:54:22 +0200

    changes to pre-install.sh and appspec.yml

--- deploy/pre-install.sh
+++ deploy/pre-install.sh
@@ -6,6 +6,7 @@ fi
 sudo rm -rf /var/lib/apt/lists/*
 sudo apt-get clean
 
+sudo apt-get update
 sudo apt-get -y install python3-pip python3-dev
 export LC_ALL="en_US.UTF-8"
 export LC_CTYPE="en_US.UTF-8"
