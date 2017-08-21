e84ec11bb1b5ea461d1919aaf6ea3c812209daf2
ruta.sakalauskaite@qustodio.com <ruta.sakalauskaite@qustodio.com>
2017-08-21 15:05:27 +0200

    changing pre-install.sh script

--- deploy/pre-install.sh
+++ deploy/pre-install.sh
@@ -7,6 +7,8 @@ sudo rm -rf /var/lib/apt/lists/*
 sudo apt-get clean
 
 sudo apt-get -y install python3-pip python3-dev
+export LC_ALL="en_US.UTF-8"
+export LC_CTYPE="en_US.UTF-8"
 pip3 install -U pytest
 pip3 install -r requirements.txt
 pip3 install eve
