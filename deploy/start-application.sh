c7430801a96cd60a26d8bb3cfea58e3b304fc341
ruta.sakalauskaite@qustodio.com <ruta.sakalauskaite@qustodio.com>
2017-08-21 18:51:31 +0200

    add mongod.conf file to start db with specified configs

--- deploy/start-application.sh
+++ deploy/start-application.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+cd /blog-api/db
+sudo bash -c "mongod --config mongod.conf > db_logs.txt 2>&1 &"
 cd /blog-api
-sudo bash -c "mongod --dbpath=/blog-api/data/db > db_logs.txt 2>&1 &"
 sudo bash -c "python3 index.py > app_logs.txt 2>&1 &"
