22009f4a9f9ff247738377cccee5198dd84da919
ruta.sakalauskaite@qustodio.com <ruta.sakalauskaite@qustodio.com>
2017-08-28 16:48:21 +0200

    add deployment requirements changes

--- deploy/post-install.sh
+++ deploy/post-install.sh
@@ -2,6 +2,8 @@
 cd /blog-api/data
 sudo mkdir db
 sudo chmod 777 -R db
-cd /blog-api/bottleneck_features
+cd /blog-api
+mkdir bottleneck_features
+cd bottleneck_features
 wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogResnet50Data.npz
 sudo service nginx restart
