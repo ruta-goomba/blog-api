71b8c3cc9f49233f120cad03f7902a71c615913a
ruta.sakalauskaite@qustodio.com <ruta.sakalauskaite@qustodio.com>
2017-08-28 17:14:17 +0200

    modify post-install.sh script

--- deploy/post-install.sh
+++ deploy/post-install.sh
@@ -2,7 +2,7 @@
 cd /blog-api/data
 sudo mkdir db
 sudo chmod 777 -R db
-cd /blog-api
+cd /blog-api/nn_models
 mkdir bottleneck_features
 cd bottleneck_features
 wget https://s3-us-west-1.amazonaws.com/udacity-aind/dog-project/DogResnet50Data.npz
